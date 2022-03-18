42e8a59f3ba946aa72cbe435220bf2e1dcb9e1c0
Gabriela Sillis <gabydias47@gmail.com>
2022-03-18 17:10:12 -0300

    feature: UI created

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Gabriela Sillis on 14/03/22.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    // Mark: - Properties
+    @IBOutlet private weak var buttonOne: UIButton!
+    @IBOutlet private weak var buttonTwo: UIButton!
+    @IBOutlet private weak var buttonThree: UIButton!
+    
+    // Mark: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
